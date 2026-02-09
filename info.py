b66142348f1235e482ad6473bcbeb45c6702fe3e
maran70 <141546842+maran70@users.noreply.github.com>
2026-02-10 00:05:32 +0530

    Update info.py

--- info.py
+++ info.py
@@ -68,8 +68,8 @@ APPROVED = environ.get("APPROVED_WELCOME", "on").lower()
 # MongoDB Configuration
 # ============================
 DATABASE_URI = environ.get('DATABASE_URI', "")
-DATABASE_URI2 = environ.get('DATABASE_URI2', "")
-DATABASE_NAME = environ.get('DATABASE_NAME', "yato")
+DATABASE_URI2 = environ.get('DATABASE_URI2', "mongodb+srv://maran117:LhTtVQOhkY43fARX@cluster0.wtofo5r.mongodb.net/?appName=Cluster0")
+DATABASE_NAME = environ.get('DATABASE_NAME', "hi")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Lucy_files')
 
 # ============================
